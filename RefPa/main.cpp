0ba875e3e376fae52047ddff70b8ae4d8197566b
tanmum <buihaitan89@gmail.com>
2014-04-07 13:45:56 -0500

    Commit

--- RefPa/main.cpp
+++ RefPa/main.cpp
@@ -7,12 +7,35 @@
 //
 
 #include <iostream>
+using namespace std;
 
+void swap(int&a, int&b)
+{
+    int c = a;
+    a = b;
+    b = c;
+}
+void swap(int*a, int*b)
+{
+    int c = *a;
+    *a = *b;
+    *b = c;
+}
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int a = 89, b = 75;
+    int c = 31, d = 44;
+    
+    cout << "Before\n";
+    std::cout << "a = " << a << "; b = " << b << endl;
+    std::cout << "c = " << c << "; b = " << d << endl;
+    
+    swap(a, b);
+    swap(&c, &d);
+    
+    cout << "After\n";
+    std::cout << "a = " << a << "; b = " << b << endl;
+    std::cout << "c = " << c << "; b = " << d << endl;
     return 0;
 }
 
